603b3e384976dc37213e0179df34c6420c09dcc6
Francisco F Gil Villalobos <franfer@Franfers-MBP.lan>
2022-06-09 07:12:25 -0500

    Working GET endpoint

---
+++ src/main/java/org/skt/microservice/SktMsApplication.java
@@ -0,0 +1,13 @@
+package org.skt.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SktMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SktMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skt/microservice/SktMsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skt.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SktMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
